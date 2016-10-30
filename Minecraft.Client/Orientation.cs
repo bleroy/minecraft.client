1fe324fee3614ae54ea6d317a94ecee7e475014a
Bertrand Le Roy <beleroy@microsoft.com>
2016-10-29 23:55:49 -0700

    Add missing enum for wood.

---
+++ Minecraft.Client/Orientation.cs
@@ -0,0 +1,10 @@
+﻿namespace Decent.Minecraft.Client
+{
+    public enum Orientation : ushort
+    {
+        UpDown = 0x0,
+        EastWest = 0x4,
+        NorthSouth = 0x8,
+        None = 0xC
+    }
+}
