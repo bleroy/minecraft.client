d68aa4f65cc799188f9985c83f5546cae039f96b
Chris Gomez <cagomez@cellbycell.com>
2016-11-10 11:03:25 -0800

    Added Emerald, Gold, and Iron blocks and ore
    Simple implementation to allow creation of these ore and blocks.

---
+++ Minecraft.Client/Blocks/Emerald.cs
@@ -0,0 +1,12 @@
+﻿namespace Decent.Minecraft.Client.Blocks
+{
+    public class Emerald : Block
+    {
+        public Emerald() : base(BlockType.Emerald) { }
+    }
+
+    public class EmeraldOre : Block
+    {
+        public EmeraldOre() : base(BlockType.EmeraldOre) { }
+    }
+}
