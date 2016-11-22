24c229cebf6b51c9a1dcb8d5c899ffc8c082c4ee
Flosus <flosus.mail@gmail.com>
2016-11-22 21:24:13 +0100

    byte is allways (> 0)

--- Decent.Minecraft.Client/Blocks/Fire.cs
+++ Decent.Minecraft.Client/Blocks/Fire.cs
@@ -9,7 +9,7 @@ namespace Decent.Minecraft.Client.Blocks
     {
         public Fire(byte intensity) : base(BlockType.Fire)
         {
-            if (intensity < 0 || intensity > 15)
+            if (intensity > 15)
             {
                 throw new ArgumentException("Fire intensity must be between 0 and 15.", "intensity");
             }
