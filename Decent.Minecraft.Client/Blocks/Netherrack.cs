d2f155a582461e8e9ae5c4f29bf3e1d749bb1502
Flosus <flosus.mail@gmail.com>
2016-11-22 16:51:09 +0100

    Adding Netherrack Block Documentation

--- Decent.Minecraft.Client/Blocks/Netherrack.cs
+++ Decent.Minecraft.Client/Blocks/Netherrack.cs
@@ -1,5 +1,23 @@
 ﻿namespace Decent.Minecraft.Client.Blocks
 {
+    /// <summary>
+    /// This it the Netherrack block found in the Nether world.<br />
+    /// <a href="http://minecraft.gamepedia.com/Netherrack">Wikipedia link</a>.
+    /// <list type="table">
+    ///   <item>
+    ///     <term>Block</term>
+    ///     <description>Netherrack</description>
+    ///   </item>
+    ///   <item>
+    ///     <term>Hardness</term>
+    ///     <description>0.4</description>
+    ///   </item>
+    ///   <item>
+    ///     <term>Tool</term>
+    ///     <description>Pickaxe</description>
+    ///   </item>
+    /// </list>
+    /// </summary>
     public class Netherrack : Block
     {
         public Netherrack() : base(BlockType.Netherrack) { }
