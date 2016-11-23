6919000054fa2ad120adeaf0539f7da382d6dddf
Flosus <flosus.mail@gmail.com>
2016-11-23 19:24:36 +0100

    Adding CoalOre and changing the CharCoal link to Block_of_Coal

--- Decent.Minecraft.Client/Blocks/Coal.cs
+++ Decent.Minecraft.Client/Blocks/Coal.cs
@@ -15,10 +15,18 @@
     }
 
     /// <summary>
-    /// <a href="http://minecraft.gamepedia.com/Coal_Ore">Gamepedia link</a>.
+    /// <a href="http://minecraft.gamepedia.com/Block_of_Coal">Gamepedia link</a>.
     /// </summary>
     public class Charcoal : Coal
     {
         public Charcoal() : base(charred: true) { }
+    }
+
+    /// <summary>
+    /// <a href="http://minecraft.gamepedia.com/Coal_Ore">Gamepedia link</a>.
+    /// </summary>
+    public class CoalOre : Block
+    {
+        public CoalOre() : base(BlockType.CoalOre) { }
     }
 }
