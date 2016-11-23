a5b6c63ff076ef02f036949f9da27c689dc3d775
Flosus <flosus.mail@gmail.com>
2016-11-23 10:33:50 +0100

    Fixing the <see> for BedHead

--- Decent.Minecraft.Client/Blocks/Bed.cs
+++ Decent.Minecraft.Client/Blocks/Bed.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     /// The bed is a multi block structure, allowing the player to sleep in the night.<br />
-    /// Beds are made out of a <see cref="BedFoot">foot</see> and a <see cref="BedFoot">head</see> part.<br />
+    /// Beds are made out of a <see cref="BedFoot">foot</see> and a <see cref="BedHead">head</see> part.<br />
     /// Breaking one of the two parts will destroy the other one and drop the bed item.<br />
     /// <a href="http://minecraft.gamepedia.com/Bed">Gamepedia link</a>.
     /// </summary>
