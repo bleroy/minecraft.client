87d1a1e2eca51b539714810cf7be6e0230a1f9de
Flosus <flosus.mail@gmail.com>
2016-11-23 21:30:39 +0100

    Removing the CharredCoal block.

--- Decent.Minecraft.Client/Blocks/Coal.cs
+++ Decent.Minecraft.Client/Blocks/Coal.cs
@@ -6,20 +6,6 @@
     public class Coal : Block
     {
         public Coal() : base(BlockType.Coal) { }
-        protected Coal(bool charred) : base(BlockType.Coal)
-        {
-            IsCharred = charred;
-        }
-
-        protected bool IsCharred { get; }
-    }
-
-    /// <summary>
-    /// <a href="http://minecraft.gamepedia.com/Block_of_Coal">Gamepedia link</a>.
-    /// </summary>
-    public class Charcoal : Coal
-    {
-        public Charcoal() : base(charred: true) { }
     }
 
     /// <summary>
